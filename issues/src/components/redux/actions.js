20f3297e4cbb3d9bcfd9949ceffa83122288b701
niko831 <nicholasnobida@yahoo.com>
2020-11-18 12:40:19 -0800

    forgot other components

--- issues/src/components/redux/actions.js
+++ issues/src/components/redux/actions.js
@@ -5,6 +5,7 @@ export const DATA_LOADING = "DATA_LOADING"
 export const ADD_POSTS = "ADD_POSTS"
 export const EDIT_POST = "EDIT_POST"
 export const DELETE_POST = "DELETE_POST"
+export const DATA_FAIL = "DATA_FAIL"
 
 export const fetchData = () => (dispatch)=>{
     axiosWithAuth()
@@ -13,4 +14,34 @@ export const fetchData = () => (dispatch)=>{
             dispatch({type: FETCH_DATA, payload: res.data})
         })
         .catch(err => console.dir(err))
+}
+
+export const signUser = (userCreds) => (dispatch) => {
+    dispatch({ type: DATA_LOADING })
+    axios
+        .post("https://bd-comake.herokuapp.com/api/auth/register", userCreds)
+            .then((res) => {
+                localStorage.setItem('token', res.data.token)
+                console.log(localStorage)
+                console.log(res)
+            })
+            .then(() => window.location = `/user/${id}`)
+            .catch(err =>
+                dispatch({type: DATA_FAIL, payload: err})
+            )
+}
+
+export const logUser = (userCreds) => (dispatch) => {
+    dispatch({ type: DATA_LOADING })
+    axios
+        .post("https://bd-comake.herokuapp.com/api/auth/login", userCreds)
+            .then((res) => {
+                localStorage.setItem('token', res.data.token)
+                console.log(localStorage)
+                console.log(res)
+            })
+            .then(() => window.location = `/user/${id}`)
+            .catch(err =>
+                dispatch({type: DATA_FAIL, payload: err})
+            )
 }
--- issues/src/components/registration/Login.js
+++ issues/src/components/registration/Login.js
@@ -1,13 +1,67 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { logUser } from "../redux/actions";
 
-const Login = () => {
-    return(
-        <div>
-            <form>
+const LogIn = (props) => {
 
-            </form>
-        </div>
-    )
-}
+  const [ loginData, setLoginData ] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+//   const [ logErrors, setLogErrors ] = useState({
+//     email: "",
+//     password: "",
+//   });
+
+  const handleLogChanges = e => {
+    const logCredentials = {
+      ...loginData,
+      [e.target.name]: e.target.value
+    };
+    setLoginData(logCredentials);
+  };
+
+  const loginSubmit = e => {
+    e.preventDefault();
+    props.logUser(loginData)
+        setLoginData({
+          email: "",
+          password: "",
+        });
+  };
+
+  return (
+    <div className="loginForm">
+        <form onSubmit={loginSubmit}>
+            <input
+                name="email"
+                type="email"
+                placeholder="Email"
+                value={loginData.email}
+                onChange={handleLogChanges}
+            />
+            {/* <br />
+            {logErrors.username.length > 0 ? (
+                <p className="error">{logErrors.email}</p>
+            ) : null}
+            <br /> */}
+            <input
+                name="password"
+                type="password"
+                placeholder="Enter your Password"
+                value={loginData.password}
+                onChange={handleLogChanges}
+            />
+            {/* <br />
+            {logErrors.password.length > 0 ? (
+                <p className="error">{logErrors.password}</p>
+            ) : null} */}
+            <br />
+            <button>Login</button>
+        </form>
+    </div>
+  );
+};
+
+export default connect(null, { logUser })(LogIn);
